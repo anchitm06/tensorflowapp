159f5602d9aeb0c74a19cab150c013b994b3e92f
anchitm06 <anchitmathur7@gmail.com>
2023-09-28 12:53:31 +0530

    revert temp commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,35 +53,34 @@ export class AppComponent implements OnInit {
       // they have a high confidence score.
       for (let n = 0; n < predictions.length; n++) {
         // If we are over 66% sure we are sure we classified it right, draw it!
-        console.log(`${predictions[n].class} - with ${Math.round(parseFloat(`${predictions[n].score}`) * 100)}% confidence.`);
-        // if (predictions[n].score > 0.66) {
-        //   const p = document.createElement('p');
-        //   p.innerText = `${predictions[n].class} - with ${Math.round(parseFloat(`${predictions[n].score}`) * 100)}% confidence.`;
-        //   p.setAttribute('style', 
-        //   `margin-left: ${predictions[n].bbox[0]}px;
-        //   margin-top: ${(predictions[n].bbox[1] - 10)}px;
-        //   width: ${(predictions[n].bbox[2] - 10)}px;
-        //   top: 0;
-        //   left: 0;`
-        //   );
+        if (predictions[n].score > 0.66) {
+          const p = document.createElement('p');
+          p.innerText = `${predictions[n].class} - with ${Math.round(parseFloat(`${predictions[n].score}`) * 100)}% confidence.`;
+          p.setAttribute('style', 
+          `margin-left: ${predictions[n].bbox[0]}px;
+          margin-top: ${(predictions[n].bbox[1] - 10)}px;
+          width: ${(predictions[n].bbox[2] - 10)}px;
+          top: 0;
+          left: 0;`
+          );
 
-        //   let highlighter = document.createElement('div');
-        //   highlighter.setAttribute('style', 
-        //   `width: ${predictions[n].bbox[2]}px;
-        //   height: ${predictions[n].bbox[2]}px;
-        //   top: ${predictions[n].bbox[1]}px;
-        //   left: ${predictions[n].bbox[0]}px;
-        //   background: rgba(0, 255, 0, 0.25);
-        //   border: 1px dashed #ffffff;
-        //   z-index: 1;
-        //   position: absolute;`
-        //   );
+          let highlighter = document.createElement('div');
+          highlighter.setAttribute('style', 
+          `width: ${predictions[n].bbox[2]}px;
+          height: ${predictions[n].bbox[2]}px;
+          top: ${predictions[n].bbox[1]}px;
+          left: ${predictions[n].bbox[0]}px;
+          background: rgba(0, 255, 0, 0.25);
+          border: 1px dashed #ffffff;
+          z-index: 1;
+          position: absolute;`
+          );
 
-        //   this.liveView.appendChild(highlighter);
-        //   this.liveView.appendChild(p);
-        //   this.children.push(highlighter);
-        //   this.children.push(p);
-        // }
+          this.liveView.appendChild(highlighter);
+          this.liveView.appendChild(p);
+          this.children.push(highlighter);
+          this.children.push(p);
+        }
       }
 
       // Call this function again to keep predicting when the browser is ready.
